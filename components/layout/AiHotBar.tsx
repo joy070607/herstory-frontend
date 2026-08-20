a6c11e39d4a0e69f93575fc27a4e83bd237a06c3
hwkong7 <hwkong7_@naver.com>
2026-08-20 22:21:22 +0900

    fix: 사이드 메뉴 아이콘 색상 통일 및 회원정보 수정 아이콘 교체
    - 여정 타임라인/스타일 추천/스마트 카트 아이콘을 currentColor로 전환해 메뉴에서는 회색, 하단 핫바에서는 기존 파란색을 유지하도록 수정
    - "회원정보 수정" 아이콘을 새 펜 아이콘(uil_pen)으로 교체

--- components/layout/AiHotBar.tsx
+++ components/layout/AiHotBar.tsx
@@ -14,16 +14,16 @@ export function AiHotBar() {
   return (
     <div className="flex items-center justify-between bg-[#E7F6FC] px-6 py-[5px]">
       <Link href={ROUTES.styleEngine} aria-label="AI 스타일 엔진">
-        <AiLogoIcon className="h-[23px] w-7" />
+        <AiLogoIcon className="h-[23px] w-7 text-sky-700" />
       </Link>
       <Link href={ROUTES.smartCart} aria-label="스마트 장바구니">
-        <DressIcon className="h-8 w-8" />
+        <DressIcon className="h-8 w-8 text-sky-700" />
       </Link>
       <Link href={ROUTES.home} aria-label="홈">
         <HomeBadgeIcon className="h-[50px] w-[50px]" />
       </Link>
       <Link href={ROUTES.journeyTimeline} aria-label="여정 타임라인">
-        <AirplaneOutlineIcon className="h-[30px] w-9" />
+        <AirplaneOutlineIcon className="h-[30px] w-9 text-sky-700" />
       </Link>
       <Link href={ROUTES.nomadMiles} aria-label="마일리지">
         <MileageIcon className="h-[31px] w-[31px]" />
